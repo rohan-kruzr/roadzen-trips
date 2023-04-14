6b4d1189953c44b7400637992fcc783aae618643
rohan-kruzr <amit@kruzr.co>
2023-04-14 18:37:56 +0530

    Added base files

---
+++ assets/geotab.js
@@ -0,0 +1,97 @@
+// /**
+// * @returns {{initialize: Function, focus: Function, blur: Function}}
+// */
+// geotab.addin.driverProfile = () => {
+//   'use strict';
+
+//   /* Scope variables */
+//   let api; // Ref https://github.com/Geotab/mg-api-js
+//   let state;
+//   let angularAppInitCheckInterval;
+
+//   /**
+//    * Initialize the add-in
+//    */
+//   let initialize = () => {
+
+//   };
+
+//   /**
+//    * Clears Angular Init check interval
+//    */
+//   let clearAngularAppinitCheck = () => {
+//       clearInterval(angularAppInitCheckInterval);
+//   };
+
+//   let onAppStart = () => {
+//     loadDriverProfileRuntime();
+//     loadDriverProfilePolyfill();
+//     loadDriverProfileMain();
+
+//       api.getSession((result) => {
+//           angularAppInitCheckInterval = setInterval(() => {
+//               if(window.myDriverProfileNgAppRef && window.myDriverProfileNgAppRef.zone){
+//                   window.myDriverProfileNgAppRef.zone.run(() => { window.myDriverProfileNgAppRef.loadGeoTabSDKData(result.database,result.sessionId,result.database); });
+//                   clearAngularAppinitCheck();
+//               }else{
+//                   console.log("DriverProfile app not ready yet, checking again");
+//               }
+//           },500)
+//       });
+//   };
+
+//   /**
+//   * Render
+//   * App Logic
+//   */
+//   let render = () => {
+//         onAppStart();
+//   }
+
+//   /**
+//    * Aborts
+//    */
+//   let abort = () => {
+//       clearAngularAppinitCheck();
+//       window.webpackJsonp = []
+//   };
+
+//   return {
+//       /*
+//       * Page lifecycle method: initialize is called once when the Add-In first starts
+//       * Use this function to initialize the Add-In's state such as default values or
+//       * make API requests (Geotab or external) to ensure interface is ready for the user.
+//       */
+//       initialize(freshApi, freshState, callback) {
+
+//           api = freshApi;
+//           state = freshState;
+
+//           initialize();
+//           if (callback) {
+//               callback();
+//           }
+//       },
+
+//       /*
+//       * Page lifecycle method: focus is called when the page has finished initialize method
+//       * and again when a user leaves and returns to your Add-In that has already been initialized.
+//       * Use this function to refresh state such as vehicles, zones or exceptions which may have
+//       * been modified since it was first initialized.
+//       */
+//       focus(freshApi, freshState) {
+//           api = freshApi;
+//           state = freshState;
+
+//           render();
+//       },
+
+//       /*
+//       * Page lifecycle method: blur is called when the user is leaving your Add-In.
+//       * Use this function to save state or commit changes to a datastore or release memory.
+//       */
+//       blur() {
+//           abort();
+//       }
+//   };
+// };
